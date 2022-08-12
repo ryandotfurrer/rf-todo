8e5b6a5fd832eaf60647d01c4a841d6116af6ece
Ryan Furrer <rfurrertech@gmail.com>
2022-08-11 20:48:47 -0400

    Added localStorage
    localStorage will remember what ToDos you added to the last on your last visit.
    
    Persists through page reload - will not persist through clearing browser cache.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,17 @@ let toDoList = [];
 const userInput = document.getElementById("user-input");
 const addToDoBtn = document.getElementById("add-todo-btn");
 const toDoListUl = document.getElementById("user-list");
+let toDoListFromLocalStorage = JSON.parse(localStorage.getItem("toDoList"));
+
+if (toDoListFromLocalStorage) {
+	toDoList = toDoListFromLocalStorage;
+	renderToDoList();
+}
 
 addToDoBtn.addEventListener("click", function () {
 	toDoList.push(userInput.value);
 	userInput.value = "";
+	localStorage.setItem("toDoList", JSON.stringify(toDoList));
 	renderToDoList();
 });
 
